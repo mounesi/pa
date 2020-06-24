9204ae22d9c1aa0591e1232d744055a70b251d2a
mounesi <armsohi@gmail.com>
2020-06-24 11:24:37 -0700

    updating streamlit app

--- src/run_streamlit.py
+++ src/run_streamlit.py
@@ -6,9 +6,9 @@ st.title("Pavement Assessment in Irvington Village in New York State")
 st.markdown(
 """
 This is a demo of a Streamlit app that shows the Roadway Condition
-geographical distribution in Irvington Village in NYS.
+geographical distribution of Irvington Village in NYS.
 
-[See source code](https://github.com/mounesi)
+[See source code](https://github.com/mounesi/pa)
 """)
 
 st.plotly_chart(fig)
